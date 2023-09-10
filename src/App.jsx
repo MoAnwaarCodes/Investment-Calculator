8638bb25cf28c84d6d9d6fc900b2bde8d512469e
Mo Anwaar Codes <moanwaar9@gmail.com>
2023-09-11 02:32:47 +0500

    huuuf Finally resolved the error

--- src/App.jsx
+++ src/App.jsx
@@ -2,59 +2,48 @@ import { useState } from "react";
 import Form from "./Component/Form";
 import Header from "./Component/Header";
 import Table from "./Component/Table";
-
 function App() {
-  // Initialize userInput state with default values or an empty object
-  const [userInput, setUserInput] = useState({
-    saving: 0,
-    yearSaving: 0,
-    intrest: 0,
-    duration: 0,
-  });
+  const [userInput, setUserInput] = useState({});
 
   const calculate_Handler = (data) => {
     setUserInput(data);
   };
+  let yearlyData = [];
+let currentSavings;
+  if (userInput) {
+     currentSavings = userInput.saving;
+    const yearlyContribution = userInput.yearSaving;
+    const expectedReturn = userInput.intrest / 100;
+    const duration = userInput.duration;
 
-  // Calculate yearlyData whenever userInput changes
-  const yearlyData = userInput
-    ? calculateYearlyData(userInput)
-    : [];
+    for (let i = 0; i < duration; i++) {
+      const yearlyInterest = currentSavings * expectedReturn;
+      currentSavings += yearlyInterest + yearlyContribution;
+      yearlyData.push({
+        year: i + 1,
+        yearlyInterest: yearlyInterest,
+        savingsEndOfYear: currentSavings,
+        yearlyContribution: yearlyContribution,
+      });
+    }
+  }
 
   return (
     <div>
-      <Header />
-      <Form calculateHandler={calculate_Handler} />
+      <Header></Header>
+      <Form calculateHandler={calculate_Handler}></Form>
 
-      {yearlyData.length > 0 ? (
-        <Table data={yearlyData} initialInvestment={userInput.saving} />
+      {userInput ? (
+        yearlyData.length > 0 ? (
+          <Table data={yearlyData} initialInvestment={currentSavings}></Table>
+        ) : (
+          <p>No investment Calculated yet.</p>
+        )
       ) : (
-        <p>No investment Calculated yet.</p>
+        <p>Waiting for user input...</p>
       )}
     </div>
   );
-}
-
-// Function to calculate yearly data based on user input
-function calculateYearlyData(userInput) {
-  const yearlyData = [];
-  let currentSavings = userInput.saving;
-  const yearlyContribution = userInput.yearSaving;
-  const expectedReturn = userInput.intrest / 100;
-  const duration = userInput.duration;
-
-  for (let i = 0; i < duration; i++) {
-    const yearlyInterest = currentSavings * expectedReturn;
-    currentSavings += yearlyInterest + yearlyContribution;
-    yearlyData.push({
-      year: i + 1,
-      yearlyInterest: yearlyInterest,
-      savingsEndOfYear: currentSavings,
-      yearlyContribution: yearlyContribution,
-    });
-  }
-
-  return yearlyData;
 }
 
 export default App;
--- src/Component/Table.jsx
+++ src/Component/Table.jsx
@@ -1,37 +1,38 @@
 import React from "react";
 
-const Table = (props) => {
- console.log(props.data[0].savingsEndofYear)
+const Table = ({ data, initialInvestment }) => {
   return (
-    
-    <table className="result">
-      <thead>
-        <tr>
-          <th>Year</th>
-          <th>Total Savings</th>
-          <th>Interest (Year)</th>
-          <th>Total Interest</th>
-          <th>Invested Capital</th>
-        </tr>
-      </thead>
-      <tbody>
-        {props.data.map((d, index) => {
-          return (
-            <tr key={index}>
-              <td>{d.year}</td>
-              <td>{d.savingsEndofYear}</td>
-              <td>{d.yearlyIntrest}</td>
-              <td>
-                {d.savingsEndofYear -
-                  props.initialInvestment -
-                  d.yearlyContribution * d.year}
-              </td>
-              <td>{d.yearlyContribution}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
+    <>
+      
+      <table className="result">
+        <thead>
+          <tr>
+            <th>Year</th>
+            <th>Total Savings</th>
+            <th>Interest (Year)</th>
+            <th>Total Interest</th>
+            <th>Invested Capital</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((d, index) => {
+            return (
+              <tr key={index}>
+                <td>{d.year}</td>
+                <td>{d.savingsEndOfYear}</td>
+                <td>{d.yearlyInterest}</td>
+                <td>
+                  {d.savingsEndOfYear -
+                    initialInvestment -
+                    d.yearlyContribution * d.year}
+                </td>
+                <td>{initialInvestment+d.yearlyContribution*d.year}</td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </>
   );
 };
 
